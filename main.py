18e2cdead378b5babb89053a51f279daf97850ed
Chris Robertson <36351411+chrobertson@users.noreply.github.com>
2023-11-16 07:11:04 -0600

    updated landing page

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'fuck you corbin man fuck'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
